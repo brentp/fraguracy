8fbf7e2529f2d827d41bde0db91adbcafc87e99c
Brent Pedersen <bpederse@gmail.com>
2023-02-16 22:30:11 -0800

    select overlapping reads

---
+++ src/main.rs
@@ -0,0 +1,212 @@
+use rust_htslib::bam::{
+    record::{Cigar, CigarStringView},
+    Read, Reader, Record,
+};
+use rustc_hash::FxHashMap;
+
+use std::env;
+use std::str;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let mut map = FxHashMap::default();
+
+    let mut bam = Reader::from_path(&args[1]).expect("error reading bam file {args[1]}");
+    bam.set_threads(3).expect("error setting threads");
+    let mut n_total = 0;
+    let mut n_pairs = 0;
+    let mut n_overlapping = 0;
+    let chroms: Vec<String> = bam
+        .header()
+        .target_names()
+        .iter()
+        .map(|n| unsafe { str::from_utf8_unchecked(n) }.to_string())
+        .collect();
+
+    bam.rc_records()
+        .map(|r| r.expect("error parsing read"))
+        .filter(|r| {
+            n_total += 1;
+            r.tid() == r.mtid()
+                && r.tid() >= 0
+                && !r.is_unmapped()
+                && !r.is_mate_unmapped()
+                && (r.pos() - r.mpos()).abs() < 1000
+                && !r.is_supplementary()
+                && !r.is_secondary()
+        })
+        .for_each(|b| {
+            let name = unsafe { str::from_utf8_unchecked(b.qname()) }.to_string();
+            if b.is_first_in_template() {
+                n_pairs += 1;
+            }
+
+            if b.pos() < b.mpos() || (b.pos() == b.mpos() && !map.contains_key(&name)) {
+                map.insert(name, b.clone());
+            } else if let Some(a) = map.remove(&name) {
+                // so we know a is before b, but we don't know if they overlap.
+                if a.cigar().end_pos() < b.pos() {
+                    return;
+                }
+                n_overlapping += 1;
+                eprintln!(
+                    "{a_tid}:{a_pos}-{a_end}({a_cigar}) <-> {b_tid}:{b_pos}-{b_end}({b_cigar})",
+                    a_tid = chroms[a.tid() as usize],
+                    a_pos = a.pos(),
+                    a_end = a.cigar().end_pos(),
+                    a_cigar = a.cigar(),
+                    b_tid = chroms[b.tid() as usize],
+                    b_pos = b.pos(),
+                    b_end = b.cigar().end_pos(),
+                    b_cigar = b.cigar(),
+                );
+                assert!(a.pos() <= b.pos());
+                eprintln!(
+                    "map len:{:?} total: {:?}, overlapping: {:?}",
+                    map.len(),
+                    n_pairs,
+                    n_overlapping
+                );
+            }
+        });
+    eprintln!(
+        "map len:{:?} total: {:?}, overlapping: {:?}, pairs: {}",
+        map.len(),
+        n_total,
+        n_overlapping,
+        n_pairs,
+    );
+}
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub struct ReadCoordinates {
+    pub start: u32,
+    pub stop: u32,
+}
+
+#[inline(always)]
+fn query(a: Cigar) -> i64 {
+    return match a {
+        Cigar::Match(n) | Cigar::SoftClip(n) | Cigar::Ins(n) | Cigar::Diff(n) | Cigar::Equal(n) => {
+            n as i64
+        }
+        _ => 0,
+    };
+}
+#[inline(always)]
+fn reference(a: Cigar) -> i64 {
+    return match a {
+        Cigar::Match(n) | Cigar::Del(n) | Cigar::Diff(n) | Cigar::Equal(n) | Cigar::RefSkip(n) => {
+            n as i64
+        }
+        _ => 0,
+    };
+}
+
+/// Return mapped parts of each read that overlap the other. Coordinates are in read-space.
+fn overlap_pieces(a: CigarStringView, b: CigarStringView) -> Vec<[ReadCoordinates; 2]> {
+    let aend = a.end_pos();
+    let bend = b.end_pos();
+    //let astart = a.pos() + a.leading_softclips();
+    //let bstart = b.pos() + b.leading_softclips();
+    if aend <= b.pos() || bend <= a.pos() {
+        return vec![];
+    }
+    let mut result: Vec<[ReadCoordinates; 2]> = Vec::new();
+    let mut ai: usize = 0;
+    let mut bi: usize = 0;
+    let mut a_genome_pos = a.pos();
+    let mut b_genome_pos = b.pos();
+    let mut a_read_pos = 0i64;
+    let mut b_read_pos = 0i64;
+    while ai < a.len() && bi < b.len() {
+        let a_genome_stop = a_genome_pos + reference(a[ai.min(a.len() - 1)]);
+        let b_genome_stop = b_genome_pos + reference(b[bi.min(b.len() - 1)]);
+        if a_genome_stop < b_genome_pos {
+            if ai < a.len() {
+                a_genome_pos += reference(a[ai]);
+                a_read_pos += query(a[ai]);
+                ai += 1;
+            }
+        } else if b_genome_stop < a_genome_pos {
+            if bi < b.len() {
+                b_genome_pos += reference(b[bi]);
+                b_read_pos += query(b[bi]);
+                bi += 1;
+            } 
+        } else { // we have some overlap.
+            // if they both consume query, we can append to our result.
+            if query(a[ai.min(b.len() - 1)]) > 0 && query(b[bi.min(b.len() - 1)]) > 0 {
+                let genome_start = a_genome_pos.max(b_genome_pos);
+                let genome_stop = a_genome_stop.min(b_genome_stop);
+                let glen = genome_stop - genome_start;
+                // glen can be 0 if, e.g. both reads end with soft-clip.
+                if glen > 0 {
+
+                    let a_over = genome_start - a_genome_pos;
+                    let b_over = genome_start - b_genome_pos;
+
+                    result.push([
+                        ReadCoordinates{start: (a_read_pos + a_over) as u32, stop: (a_read_pos + a_over + glen) as u32},
+                        ReadCoordinates{start: (b_read_pos + b_over) as u32, stop: (b_read_pos + b_over + glen) as u32},
+                    ])
+                }
+
+            }
+            // we had some overlap. now we increment the lowest genome pos by end.
+            if a_genome_stop <= b_genome_stop && ai < a.len() {
+                a_genome_pos += reference(a[ai]);
+                a_read_pos += query(a[ai]);
+                ai += 1;
+
+            }
+            if b_genome_stop <= a_genome_stop && bi < b.len() {
+                b_genome_pos += reference(b[bi]);
+                b_read_pos += query(b[bi]);
+                bi += 1;
+            }
+
+
+
+
+
+
+
+
+        }
+    }
+
+    return result;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rust_htslib::bam::record::{Cigar, CigarString};
+
+    #[test]
+    fn test_simple_overlap() {
+        let a = CigarString(vec![Cigar::Match(10), Cigar::Match(80), Cigar::SoftClip(10)]).into_view(8);
+        let b = CigarString(vec![Cigar::Match(70), Cigar::Match(40), Cigar::SoftClip(10)]).into_view(5);
+
+        let r = overlap_pieces(a, b);
+
+        let expected = [
+            [
+                ReadCoordinates { start: 0, stop: 10, },
+                ReadCoordinates { start: 3, stop: 13, },
+            ],
+            [
+                ReadCoordinates { start: 10, stop: 67, },
+                ReadCoordinates { start: 13, stop: 70, },
+            ],
+            [
+                ReadCoordinates { start: 67, stop: 90, },
+                ReadCoordinates { start: 70, stop: 93, },
+            ],
+        ];
+
+        dbg!(&r);
+        assert_eq!(r, expected);
+    }
+}
