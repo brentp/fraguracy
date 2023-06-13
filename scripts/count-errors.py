4339e0dd1c9e39dd29e3cc0ba74b7246d603f67c
Brent Pedersen <bpederse@gmail.com>
2023-06-13 14:38:30 -0600

    bugs

--- scripts/count-errors.py
+++ scripts/count-errors.py
@@ -24,36 +24,44 @@ def main(vcf_file: str, *, prefix: str = "error-counts"):
             continue
 
         error_samples = variant.INFO.get('dn').split(',')
-        frag_count = variant.INFO.get('fraguracy_count', 0)
+        frag_count = int(variant.INFO.get('fraguracy_count', 0))
         lcr = bool(variant.INFO.get('LCR', 0))
         #frag_count = variant.INFO.get('fraguracy_samples', 0)
         snp = 'snp' if variant.is_snp else 'indel'
         for s in error_samples:
             d[snp][s][frag_count] += 1
             lcr_d[snp][s][lcr] += 1
-            counts_by_sample[s] += 1
+            if snp == 'snp':
+                counts_by_sample[s] += 1
+
+    skip_samples = {s for s, cnt in counts_by_sample.items() if cnt > 1500}
+    print({s: counts_by_sample[s] for s in skip_samples})
 
     max_count = 50
     # now print out the snp and indel dictionaries to separate files
     for snp in ['snp', 'indel']:
         with open(f'{prefix}.{snp}.errors', 'w') as f:
-            f.write("sample\tsnp\tcutoff\tcount\n")
+            f.write("sample\tsnp\tcutoff\tcount_at_or_above\tcount\n")
             for sample in d[snp]:
+                if sample in skip_samples: continue
                 # we want to count all the errors
                 cur_keys = [c for c in d[snp][sample].keys() if c >= max_count]
                 cum_sum = sum([d[snp][sample][c] for c in cur_keys])
-                f.write(f'{sample}\t{snp}\t{max_count}\t{cum_sum}\n')
+                f.write(f'{sample}\t{snp}\t{max_count}\t{cum_sum}\t{cum_sum}\n')
                 for i in range(max_count - 1, -1, -1):
-                    cum_sum += d[snp][sample].get(i, 0)
-                    f.write(f'{sample}\t{snp}\t{i}\t{cumsum}\n')
+                    cnt = d[snp][sample].get(i, 0)
+                    cum_sum += cnt
+                    f.write(f'{sample}\t{snp}\t{i}\t{cum_sum}\t{cnt}\n')
+                if snp == 'snp':
+                    assert(cum_sum == counts_by_sample[sample])
 
         with open(f'{prefix}.{snp}.lcr', 'w') as f:
             f.write("sample\tsnp\tlcr\tcount\n")
             for sample in lcr_d[snp]:
+                if sample in skip_samples: continue
                 # we want to count all the errors
                 f.write(f'{sample}\t{snp}\ttrue\t{lcr_d[snp][sample][True]}\n')
                 f.write(f'{sample}\t{snp}\tfalse\t{lcr_d[snp][sample][False]}\n')
-    print(dict(counts_by_sample))
 
 if __name__ == '__main__':
     defopt.run(main)
